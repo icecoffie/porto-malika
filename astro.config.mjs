62e169be990083b5094c915b9777e5beafa6b5b0
Malika Shakila <154298755+icecoffie@users.noreply.github.com>
2025-12-30 15:01:54 +0700

    Update Astro configuration for site and adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://malika.engineer",
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
     resolve: {
       alias: {
@@ -15,7 +19,7 @@ export default defineConfig({
       },
     },
   },
-  output: "static",
+  output: "server",
   build: {
     inlineStylesheets: "auto",
   },
